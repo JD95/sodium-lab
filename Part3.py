92a85b753f44c415ecc1ce7db08215c688197f17
Jeffrey Dwyer <Jeffrey Dwyer>
2017-10-27 17:58:25 -0700

    Fixed bug in part3

--- Part3.py
+++ Part3.py
@@ -1,33 +1,45 @@
-#~/usr/bin/env python
+#!~/usr/bin/env python
 
 import socket
-import nacl.utils
+import binascii
 from nacl.public import PrivateKey, Box
-from nacl.encoding import Base64Encoder
+from nacl.encoding import Base16Encoder, HexEncoder
+import base64
 
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("cyberlab.pacific.edu", 12001))
-    totalsent = 0
+    s.connect(('cyberlab.pacific.edu', 12001))
     private = PrivateKey.generate()
     public = private.public_key
-    box = Box(private, public)
-    msg = bytearray('CRYPTO 1.0 REQUEST\r\n'
-                    + 'Name: Jeff Dwyer\r\n'
-                    + 'PublicKey: ' +
-                    str(public.encode(Base64Encoder)) + '\r\n'
-                    + '\r\n', 'utf-8')
+
+    k = public.encode(encoder=Base16Encoder)
+    msg = (b'CRYPTO 1.0 REQUEST\r\n'
+           + b'Name: Jeff Dwyer\r\n'
+           + b'PublicKey: ' + k + b'\r\n'
+           + b'\r\n')
+
+    # Sending Request
+    print(msg.decode('utf-8'))
+    totalsent = 0
     while totalsent < len(msg):
         sent = s.send(msg[totalsent:])
         if sent == 0:
             raise RuntimeError('socket connection broken')
         totalsent += sent
+
     chunks = []
-    bytes_recd = 0
-    while bytes_recd < len(msg):
-        chunk = s.recv(min(len(msg) - bytes_recd, 2048))
-        if chunk == b'':
-            raise  RuntimeError('socket connection broken')
-        chunks.append(chunk)
-        bytes_recd += len(chunk)
-    print(box.decrypt(b''.join(chunks)))
+    listen = True
+    while listen:
+        chunk = s.recv(2048)
+        chunks.extend(chunk)
+        listen = chunk != b''
+
+    c = bytearray(chunks).decode('utf-8')
+    server_public = c.split('\r\n')[2].split(': ')[1]
+    c = c.split('\r\n')[3].split(': ')[1]
+    dumb = PrivateKey.generate()
+    dumb.public_key._public_key = base64.b16decode(server_public)
+    b = Box(private, dumb.public_key)
+    c = c.encode('utf-8')
+    c = binascii.unhexlify(c)
+    print(b.decrypt(c))
